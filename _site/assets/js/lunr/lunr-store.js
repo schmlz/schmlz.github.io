ff20ade32ca90ac7a48a6e2b2badbf1cfcd8733b
Johannes Schmalz <johannes.schmalz@anu.edu.au>
2022-06-10 13:48:19 +1000

    cleaning up _site

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "CoGNeRe – Probabilistic Replanning with Guarantees",
-        "excerpt":"CoGNeRe is solver for probabilistic shortest path problems. Akin to other replanners like Robust-FF, CoGNeRe constructs a solution to the probabilistic problem by solving deterministic relaxations, which lets it return partial policies quickly in an online fashion. The novelty is that by using column generation, it can provide guarantees of...","categories": [],
-        "tags": [],
-        "url": "/cognere/",
-        "teaser": "/assets/images/cognere/cognere_summary.svg"
-      }]
